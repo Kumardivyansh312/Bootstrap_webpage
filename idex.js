f72aab2053cf780616b01cfa6f002d480ff520ff
Divyansh <kumar.divyansh312@yahoo.com>
2021-04-19 12:47:12 +0530

    pages added

---
+++ idex.js
@@ -0,0 +1,10 @@
+let box = $("#searchBox").value;
+let btn = document.getElementById("searchBtn");
+
+btn.click(function(){
+   
+}) 
+btn.onclick(function(){
+    href("https://www.google.com")
+})
+    
